8b46a2efd6af0ab5c5b4f13bc9d2a7c9af9ab8cd
Timo Laakkonen <timo.laakkonen@edu.savonia.fi>
2022-02-18 02:54:32 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,55 @@ function App() {
   
   useEffect(() => {
 
-const df = new DataFrame({'pig': [20, 18, 489, 675, 1776],
-                               'horse': [4, 25, 281, 600, 1900]}, {index: [1990, 1997, 2003, 2009, 2014]})
-        df.plot("plot_div").bar()
+const df = new DataFrame({'walk': [20, 18, 489, 675, 1776],
+                               'ride': [4, 25, 281, 600, 1900]}, {index: [1990, 1997, 2003, 2009, 2014]})
+
+
+ const layoutXXX = {
+          title: {
+            text: "Time series plot of AAPL open and close points",
+            x: 0
+          },
+          legend: {
+            bgcolor: "#fcba03",
+            bordercolor: "#444",
+            borderwidth: 1,
+            font: { family: "Arial", size: 10, color: "#fff" }
+          },
+          width: 1000,
+          yaxis: {
+            title: 'AAPL open points',
+          },
+          xaxis: {
+            title: 'Date',
+          },
+        }
+
+const layout = {
+ 
+          width: 1000,
+          plot_bgcolor:"#00BBAA",
+          paper_bgcolor:"#00BB55",
+          yaxis: {
+            title: '1000 km',
+          },
+          xaxis: {
+            title: 'Year',
+          }          
+}
+
+const config = {
+}
+
+        const configXXX = {
+          columns: ["AAPL.Open", "AAPL.Close"], //columns to plot
+          displayModeBar: true,
+          displaylogo: false,
+        }
+        //df.plot("plot_div").line({ layout, config })
+
+
+        df.plot("plot_div").bar({ layout, config })
 
 /*    
 readCSV("https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/data/titanic.csv")
